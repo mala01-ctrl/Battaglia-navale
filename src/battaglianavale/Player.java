66eeddac93cd541e981f8d0937163a9b2e5d3642
Lorenzo <malavoltilorenzo0@gmail.com>
2019-11-27 17:36:41 +0100

    modifica del costruttore e aggiunta dei commenti

--- src/battaglianavale/Player.java
+++ src/battaglianavale/Player.java
@@ -5,21 +5,46 @@
  */
 package battaglianavale;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author informatica
  */
 public class Player implements Runnable{
+    /**
+     * Variabile di tipo Socket utilizzata per instaurare la 
+     * connessione con il client
+     */
     private Socket socket;
+    /**
+     * Rapprsenta il nome del giocatore 
+     */
     private String nomeGiocatore;
+    /**
+     * Variabile utilizzata per ricevere qualcosa in input
+     */
     private Scanner input;
+    /**
+     * Variabile utilizzata per la comunicazione con il client
+     */
     private PrintWriter output;
+    /**
+     * Matrice di interi che rappresenta la matrice della battaglia navale
+     */
     private int[][] matrice;
     
+    /**
+     * Costruttore della classe player 
+     * @param socket socket per instaurare la connessione con il client
+     * @param nomeGiocatore nome del giocatore
+     */
     public Player(Socket socket, String nomeGiocatore)
     {
         this.socket = socket;
@@ -38,6 +63,12 @@ public class Player implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Hello");
+        System.out.println("Connesso: " + socket);
+        try {
+            output = new PrintWriter(socket.getOutputStream(), true);
+            output.println(new Date().toString());
+        } catch (IOException ex) {
+            Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
